6b3cde4dca0308455a40dc392f5c73bd72eb3454
mjones2 <mjones2@11cc03de-f2ae-44ad-bdce-4d255cae5061>
2010-07-15 20:26:27 +0000

    minor tweaks to text on index page. added demo links
    git-svn-id: https://svn.lis.illinois.edu/NEMA/m2k/mirex/e6k@1272 11cc03de-f2ae-44ad-bdce-4d255cae5061

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php 
-	/*
-		UserCake Version: 1.4
-		http://usercake.com
-		
-		Developed by: Adam Davis
-	*/
 	require_once("models/config.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -24,18 +18,21 @@
         
         <div id="main">
             <h1>Welcome to Evalutron 6000</h1>
-				<p>In order to use the Evalutron 6000 you will need to modern web browser (e.g., Firefox, Internet Explorer, Safari, Mozilla, etc) that supports JavaScript, Flash, and Cookies. Evalutron has been tested on Windows XP, Windows 7, MacOS X, and Ubuntu Linux. If you are using a different platform and having trouble, please try accessing Evalutron 6000 from another machine. If you are still having difficulty, contact mirex@imirsel.org.</p>
-				<p>If you have an account with the MIREX submission system, then you can use the same account for Evalutron 6000. Otherwise, you must register a new account. Click on the "Register" link on the left side of the page to create an account.</p>
+				<p>In order to use the Evalutron 6000 you will need a modern web browser that supports <strong>JavaScript, Flash, and Cookies</strong>. Evalutron has been tested on several popular browsers, and should work for you. If you are having trouble, please try another browser or machine. If you are still having difficulty, contact mirex@imirsel.org.</p>
+				<p>If you have an account with the MIREX submission system, then you can use the same account for Evalutron 6000. Otherwise, you must register a new account. Click on the "Register" link to the left to create an account.</p>
 
 			<h2>Walkthrough</h2>
 				<p>There is a full walkthrough document available at: <a href="http://www.music-ir.org/mirex/wiki/2010:Evalutron6000_Walkthrough">http://www.music-ir.org/mirex/wiki/2010:Evalutron6000_Walkthrough</a>. Please read it prior to starting your evaluation.</p>
+			
+			<h2>Demo</h2>
+				<p>If you want to see how E6K works, but aren't ready to commit to evaluating, <a href="demo.php">try the demo</a>. The demo has just 5 candidates to evaluate, and the results are not saved. (And yes, those are "real" results, from AMS 2009).</p>
 
 			<h2>Important notices</h2>
 			<ol>
-				<li>This system is NOT open to the general public. Human Subjects regulations allow us to accept only graders who have a stake in Music Information Retrieval and Music Digital Library research.</li>
-				<li>Do NOT click "Get Assignments" link if you are merely curious to see what the Evalutron 6000 is set up to do. You will disrupt the distribution of assignments to other graders.</li>
-				<li>The Evalutron is set up for both Audio Music Similarity (AMS) task and Symbolic Melodic Similarity (SMS) task. Please be careful about what task you are selecting to evaluate for.</li>
-				<li>Please do NOT create multiple accounts for yourself Evalutron. If you can't log in, use the "Forgot Password" link found at the left side of the homepage (Fig 1).</li>
+				<li>This system is NOT open to the general public. Human subjects research regulations allow us to accept only graders who are part of the MIR and MDL community.</li>
+				<li>Do NOT request assignments if you are merely curious to see how E6K works. You will disrupt the distribution of assignments to real graders. <strong><a href="demo.php">Try the E6K Demo</a> instead!</strong></li>
+				<li>The Evalutron is set up for <strong>both</strong> Audio Music Similarity (AMS) task and Symbolic Melodic Similarity (SMS) task. Please be careful about what task you are selecting to evaluate for.</li>
+				<li>Please do NOT create multiple accounts. If you can't log in, use the "Forgot Password" link found at the left menu.</li>
 			</ol>
             <div class="clear"></div>
         </div>
