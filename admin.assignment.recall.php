60199e300949b27afcab030052eaf3443a4973dc
mjones2 <mjones2@11cc03de-f2ae-44ad-bdce-4d255cae5061>
2010-07-20 02:02:45 +0000

    recall script
    git-svn-id: https://svn.lis.illinois.edu/NEMA/m2k/mirex/e6k@1289 11cc03de-f2ae-44ad-bdce-4d255cae5061

---
+++ admin.assignment.recall.php
@@ -0,0 +1,69 @@
+<?php
+	/*
+		UserCake Version: 1.4
+		http://usercake.com
+		
+		Developed by: Adam Davis
+	*/
+	require_once("models/config.php");
+	
+	//Prevent the user visiting the logged in page if he/she is not logged in
+	if (!isUserLoggedIn()) { header("Location: login.php"); die(); }
+	if (!$loggedInUser->isGroupMember(2)) { die(); }
+		
+	if ((isset($_POST['confirm'])) && 
+		(strtoupper($_POST['confirm']) == "YES") &&
+		(isset($_POST['task'])) &&
+		(preg_match("/^[0-9]+$/", $_POST['task'])) &&
+		(isset($_POST['query']))) {
+		$tid = $_POST['task'];
+		$query = $_POST['query'];
+
+		adminRecallQuery($loggedInUser, $tid, $query);
+		header("Location: admin.assignments.php");
+	}
+	
+	if ((!isset($_GET['t'])) || (!isset($_GET['q']))) {
+		header("Location: admin.assignments.php");
+	}
+	$tid = $_GET['t'];
+	$query = $_GET['q'];
+	
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MIREX :: E6K ADMIN :: Recall Query</title>
+<link href="mirex.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="wrapper">
+
+	<div id="content">
+    
+        <div id="left-nav">
+        <?php include("layout_inc/left-nav.php"); ?>
+            <div class="clear"></div>
+        </div>
+        
+        <div id="main">
+        	<h2>Confirm recall of query <?php echo $query;?></h2>
+        	<p>This will delete all ratings made on this query and return the query to the pool
+        	for assignment to another grader. Are you sure you wish to do this?</p>
+        	<p>This action CANNOT be undone</p>
+			<form action="admin.assignment.recall.php" method="post">
+				<input type="hidden" name="task" value="<?php echo $tid;?>"/>
+				<input type="hidden" name="query" value="<?php echo $query;?>"/>
+				<input type="submit" name="confirm" value="Yes" />
+				<input type="button" value="No" onclick="window.location.href='admin.assignments.php'"/>
+			</form>
+		
+		</div>
+	</div>
+</div>
+</body>
+</html>
+<?php
+	
+	
