8b5d9c1d96989169899bec266cf1200027005a38
mjones2 <mjones2@11cc03de-f2ae-44ad-bdce-4d255cae5061>
2010-07-20 01:12:54 +0000

    confirmation added to signup flow
    git-svn-id: https://svn.lis.illinois.edu/NEMA/m2k/mirex/e6k@1285 11cc03de-f2ae-44ad-bdce-4d255cae5061

--- assignment.get.php
+++ assignment.get.php
@@ -10,13 +10,59 @@
 	//Prevent the user visiting the logged in page if he/she is not logged in
 	if (!isUserLoggedIn()) { header("Location: login.php"); die(); }
 
+
 	if ((!empty($_POST)) && (isset($_POST['task'])))
 	{
-		if (!userHasGivenConsent($loggedInUser)) { header("Location: consent.php?assignTask=".$_POST['task']); die(); }
 		$tid = $_POST['task'];
-		$assignments = userGetAssignments($loggedInUser, $tid);
-		if (count($assignments) == 0) {
-			$assignments = userAssignQueries($loggedInUser, $tid);
+
+		if ((isset($_POST['confirm'])) && (strtoupper($_POST['confirm']) == "YES")) {
+			if (!userHasGivenConsent($loggedInUser)) { header("Location: consent.php?assignTask=".$_POST['task']); die(); }
+			$assignments = userGetAssignments($loggedInUser, $tid);
+			if (count($assignments) == 0) {
+				$assignments = userAssignQueries($loggedInUser, $tid);
+			}
+			header("Location: assignment.list.php"); die();
 		}
+		else {
+			$task = getTask($tid);
+		?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MIREX :: E6K :: <?php echo $loggedInUser->display_username; ?></title>
+<link href="mirex.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="wrapper">
+
+	<div id="content">
+    
+        <div id="left-nav">
+        <?php include("layout_inc/left-nav.php"); ?>
+            <div class="clear"></div>
+        </div>
+        
+        <div id="main">
+        	<h2>Confirm signup</h2>
+        	<p>Are you certain you wish to sign up for <strong><?php echo stripslashes($task['task_Name']);?></strong>?</p>
+        	<p>This action CANNOT be undone</p>
+			<form action="assignment.get.php" method="post">
+				<input type="hidden" name="task" value="<?php echo $tid;?>"/>
+				<input type="submit" name="confirm" value="Yes" />
+				<input type="button" value="No" onclick="window.location.href='assignment.list.php'"/>
+			</form>
+		
+		</div>
+	</div>
+</div>
+</body>
+</html>
+<?php
+		}	
 	}
-	header("Location: assignment.list.php"); die();
+	else {
+		header("Location: assignment.list.php"); die();
+	}
+	
+	
