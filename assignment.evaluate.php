4c00b0b69724176f912881635e4c34150a6d7b9b
gzhu1 <gzhu1@11cc03de-f2ae-44ad-bdce-4d255cae5061>
2011-09-29 21:49:49 +0000

    remove an extra line
    git-svn-id: https://svn.lis.illinois.edu/NEMA/m2k/mirex/e6k@1317 11cc03de-f2ae-44ad-bdce-4d255cae5061

--- assignment.evaluate.php
+++ assignment.evaluate.php
@@ -176,7 +176,6 @@ JSON = {
 							<div style="font-weight:bold"><?php echo enhash($c['result_Candidate'], "beer");?></div>
 							<a type="audio/mpeg" href="<?php echo genMP3URL($task['task_MP3'], $query, $c['result_Candidate']);?>test">Query</a><br/>
 							<a type="audio/mpeg" href="<?php echo genMP3URL($task['task_MP3'], $c['result_Candidate'], NULL);?>">Candidate</a>
-							<?php echo $c['result_Candidate'];?>
 						</td>
 						<td align="center">
 							<table width="175px">
